bf60f22ddafb8fdc1230ab179a7f408fd9d30051
ConnorMerrell <138818788+ConnorMerrell@users.noreply.github.com>
2023-09-10 19:44:26 +0100

    Update SeperatingCategories.py

--- SeperatingCategories.py
+++ SeperatingCategories.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
+categoryName = "season"
+
 data = pd.read_csv('SeperatingCategories.csv')
 print(data.head())
-categories = data.season.unique()
+categories = data[categoryName].unique()
 #print(categories)
 
 for I in categories:
-    df2 = data.loc[data['season'] == I]
+    df2 = data.loc[data[categoryName] == I]
     #print(df2)
     filename = "output//" + I + ".csv"
     #print(filename)
